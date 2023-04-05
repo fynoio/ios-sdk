cfd665a84e16322c7a9d49a993badf16835095c0
Khush Chandawat <khush@fyno.io>
2023-04-05 19:21:05 +0530

    app category setup3

---
+++ Sources/FYNOSDK/FYNOService.swift
@@ -0,0 +1,28 @@
+#if os(iOS)
+//
+//  File.swift
+//  
+//
+//  Created by Khush Chandawat on 05/04/23.
+//
+
+import Foundation
+import UserNotifications
+
+public class FYNOService{
+    public init() {}
+    
+    public static func handleDidReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        let bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        guard let content = bestAttemptContent,
+              let attachmentURLString = content.userInfo["urlImageString"] as? String,
+              let attachmentURL = URL(string: attachmentURLString) else {
+            contentHandler(request.content)
+            return
+        }
+        
+        Utilities.downloadImageAndAttachToContent(from: attachmentURL, content: content, completion: contentHandler)
+    }
+}
+#endif
