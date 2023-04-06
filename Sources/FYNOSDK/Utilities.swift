41f0fb30ff0ef94b4495ddee54392a8d5605e6e0
Khush Chandawat <khush@fyno.io>
2023-04-06 20:20:49 +0530

    user profile support

--- Sources/FYNOSDK/Utilities.swift
+++ Sources/FYNOSDK/Utilities.swift
@@ -109,7 +109,7 @@ public class Utilities{
                 guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
                     completionHandler(.failure(NSError(domain: "Invalid status code", code: -1, userInfo: nil )))
                     let httpResponse = response as? HTTPURLResponse
-                         print(httpResponse.statusCode)
+                         print(httpResponse??.statusCode)
                     
                     return
                 }
