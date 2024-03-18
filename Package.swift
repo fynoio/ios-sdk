697a565803e0153d49ddba287a163f29684c1797
Viram Jain <viram@fyno.io>
2024-03-18 15:39:29 +0530

    added inapp in ios sdk

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "fyno", type: .dynamic,                                                                                                       
+            name: "fyno", type: .dynamic,                                                                                                             
             targets: ["fyno"]),
     ],
     dependencies: [
